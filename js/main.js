7f5eb04ff37ae7bd561d45f4d0b49110d3ac590d
le2o8w <48060842+le2o8w@users.noreply.github.com>
2019-10-24 22:29:20 +0200

    Added favicons

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
+function scroll(element) {
+  window.scrollTo({
+    behavior: "smooth",
+    left: 0,
+    top: element.offsetTop
+  });
+}
+
 (function() {
   var burger = document.querySelector(".burger-container");
   var nav = document.querySelector("nav.menu");
   var bar = document.querySelectorAll(".bar");
-
+  var button = document.getElementById("discover");
   var form = document.getElementById("contactForm");
 
   burger.onclick = function() {
@@ -12,23 +20,25 @@
     bar[1].classList.toggle("rotateA");
     nav.classList.toggle("menu-opened");
   };
-
-  form.addEventListener("submit", event => {
-    var name = document.getElementById("name").value;
-    var email = document.querySelector("#email");
-    var message = document.querySelector("#message");
-    event.preventDefault();
-    console.log("%C" + name, "");
-  });
+  if (form) {
+    form.addEventListener("submit", event => {
+      var name = document.getElementById("name").value;
+      var email = document.querySelector("#email");
+      var message = document.querySelector("#message");
+      event.preventDefault();
+      console.log("%c" + name + "\n" + email + "\n" + message, "background-color:#f8f4f3; color:#000; padding:10px 5px");
+    });
+  }
+  if (button) {    
+    button.addEventListener("click", event => {
+      event.preventDefault();
+      let news = document.getElementById("news");
+      scroll(news);
+    });
+  }
   document.getElementById("top").addEventListener("click", () => {
     let body = document.querySelector("body");
     scroll(body);
   });
 })();
-function scroll(element) {
-  window.scrollTo({
-    behavior: "smooth",
-    left: 0,
-    top: element.offsetTop
-  });
-}
+
